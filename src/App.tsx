1e7ecffe95dc7f7da9b9b5d7d701175f5b45616a
Jay Garvey <jay.garvery@outlook.com>
2025-10-14 10:14:02 -0400

    Add basename and force remount for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import { ThemeProvider } from './contexts/ThemeContext';
 import Header from './components/Header';
@@ -10,23 +10,31 @@ import RewardsOptimizer from './pages/RewardsOptimizer';
 import PaymentTracking from './pages/PaymentTracking';
 import Settings from './pages/Settings';
 
+function AppContent() {
+  const location = useLocation();
+  
+  return (
+    <div className="App">
+      <Header />
+      <main className="main-content">
+        <Routes key={location.pathname}>
+          <Route path="/" element={<IssuerSelection />} />
+          <Route path="/issuer/:issuerName" element={<CardSelection />} />
+          <Route path="/my-cards" element={<MyCards />} />
+          <Route path="/rewards-optimizer" element={<RewardsOptimizer />} />
+          <Route path="/payment-tracking" element={<PaymentTracking />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
 function App() {
   return (
     <ThemeProvider>
-      <Router>
-        <div className="App">
-          <Header />
-          <main className="main-content">
-            <Routes>
-              <Route path="/" element={<IssuerSelection />} />
-              <Route path="/issuer/:issuerName" element={<CardSelection />} />
-              <Route path="/my-cards" element={<MyCards />} />
-              <Route path="/rewards-optimizer" element={<RewardsOptimizer />} />
-              <Route path="/payment-tracking" element={<PaymentTracking />} />
-              <Route path="/settings" element={<Settings />} />
-            </Routes>
-          </main>
-        </div>
+      <Router basename="/credit-card-tracker">
+        <AppContent />
       </Router>
     </ThemeProvider>
   );
